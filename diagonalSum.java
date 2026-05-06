4b44777ddc445be5ef373f88ffcfd8156ff8ec3d
Abhishek Pandey <abhishekpandeymgic7@gmail.com>
2026-05-07 00:38:11 +0530

    4

--- diagonalSum.java
+++ diagonalSum.java
@@ -3,8 +3,8 @@ class Solution {
         int sum = 0;
         int n = mat.length;
         for(int i = 0; i<n;i++ ){
-                    sum += mat[i][i];              
-                    sum += mat[i][n-1-i];
+                sum += mat[i][i];              
+                sum += mat[i][n-1-i];   
         }
         if(n%2== 1){
             sum -= mat[n/2][n/2];
